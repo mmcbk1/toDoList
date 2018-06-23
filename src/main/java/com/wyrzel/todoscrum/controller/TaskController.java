0c7e39a7f5f179e6dbdb68509ce23badc5e211df
Wyrzel <Wyrzel86>
2018-06-23 14:52:56 +0200

    duuupa

--- src/main/java/com/wyrzel/todoscrum/controller/TaskController.java
+++ src/main/java/com/wyrzel/todoscrum/controller/TaskController.java
@@ -3,10 +3,9 @@ package com.wyrzel.todoscrum.controller;
 import com.wyrzel.todoscrum.entity.Task;
 import com.wyrzel.todoscrum.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class TaskController {
@@ -25,5 +24,16 @@ public class TaskController {
     public Task postTask(@RequestBody Task task)
     {
         return taskRepository.save(task);
+    }
+
+
+    @PutMapping(path= "task/{id}/status")
+    public Task putTask(@RequestBody Task task, @PathVariable Long id)
+    {
+        Optional<Task> taskOptional= taskRepository.findById(id);
+        Task updatedTask= taskOptional.get();
+        updatedTask.setStatus(task.getStatus());
+
+        return taskRepository.save(updatedTask);
     }
 }
